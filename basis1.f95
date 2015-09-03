30d60fb8dff2465856db5ed2aedae185068ad7f2
Fausto Saporito <fausto.saporito@gmail.com>
2015-09-03 19:54:09 +0200

    genPauli

--- basis1.f95
+++ basis1.f95
@@ -2,7 +2,7 @@
 !
 module basis
 use types
-use constants, only: I
+use constants, only: I, pi
 use strings
 use fact
 use dispmodule
@@ -17,7 +17,7 @@ end type Hbas
 
 type Ubas ! Unitary basis
     complex(dp), dimension(:,:), allocatable :: matrix
-    character(7) idx
+    character(8) idx
     logical :: unitary = .true.
     logical :: hermitian = .false.
 end type Ubas
@@ -31,6 +31,20 @@ type UBasis
     type(Ubas), dimension(:), allocatable :: bas
     integer dimen
 end type UBasis
+
+type comp
+    character(6) idx
+    real :: x
+end type comp
+
+type components
+    type(comp), dimension(3) :: cmp
+end type components
+
+interface printBasis
+    module procedure printHbasis
+    module procedure printUbasis
+end interface
 
 contains
 
@@ -106,6 +120,65 @@ endif
 
 end function h
 
+function clockm(d) result(rr)
+    integer, intent(in) :: d
+    complex(dp), dimension(d,d) :: rr
+    complex(dp) :: omega,ompar
+    integer :: i
+
+    ompar=complex(0d0,2.0d0*pi/d)
+    omega=zexp(ompar)
+
+    rr=0
+    forall (i=0:d-1) rr(i+1,i+1)=omega**i
+end function clockm
+
+function skewm(d) result(rr)
+    integer, intent(in) :: d
+    complex(dp), dimension(d,d) :: rr
+    integer :: i
+
+    rr=0
+    rr(1,d)=1
+    forall (i=1:d-1) rr(i+1,i)=1
+end function skewm
+
+function genPauli(j,k,d) result(rr)
+    integer, intent(in) :: j,k,d
+    integer :: i
+    complex(dp), dimension(d,d) :: rr,cm,sm,t1,t2,t3,t4
+
+    cm=clockm(d)
+    sm=skewm(d)
+
+    t1=cm
+    t2=sm
+    t3=cm
+    t4=sm
+
+    if ((j==1) .and. (k==1)) then
+        forall(i=1:d) rr(i,i)=1
+    else if (j==1) then
+        do i=1,d
+            t3=matmul(t3,cm)
+        end do
+        rr=t3
+    else if (k==1) then
+        do i=1,d
+            t4=matmul(t4,sm)
+        end do
+        rr=t4
+    else if ((k>1) .and. (j>1)) then
+        do i=1,k
+            t1=matmul(t1,cm)
+        end do
+        do i=1,j
+            t2=matmul(t2,sm)
+        end do
+        rr=matmul(t1,t2)
+    end if
+end function genPauli
+
 function genHbasis(d) result(rrbas)
     integer, intent(in) :: d
     type(Hbas) :: b1
@@ -155,6 +228,43 @@ function genHbasis(d) result(rrbas)
     print *,'Done generating basis.'
 end function genHbasis
 
+function genUbasis(d) result(rrbas)
+    integer, intent(in) :: d
+    type(Ubas) :: b1
+    type(Ubasis) :: rrbas
+    integer :: i,j,counti,ed
+    character(len=2) c,c1,c2
+
+    ed=d**2
+    allocate(rrbas%bas(ed))
+
+    print *,'Generating unitary basis ...'
+
+    do i=1,ed
+        allocate(rrbas%bas(i)%matrix(d,d))
+    end do
+
+    call writenum(d,c1,'i2')
+    call removesp(c1)
+    counti=1
+
+    do i=1,d
+        do j=1,d
+                b1%matrix=genPauli(i,j,d)
+                call writenum(i,c,'i2')
+                call writenum(j,c2,'i2')
+                call removesp(c)
+                call removesp(c2)
+                b1%idx='S('//c//','//c2//')'
+                call removesp(b1%idx)
+                rrbas%bas(counti)=b1
+                counti=counti+1
+        end do
+    end do
+    rrbas%dimen=d
+    print *,'Done generating basis.'
+end function genUbasis
+
 subroutine printHbasis(bb)
     type(Hbasis), intent(in) :: bb
     integer i,ed
@@ -166,5 +276,36 @@ subroutine printHbasis(bb)
     end do
 
     return
-end subroutine
+end subroutine printHbasis
+
+subroutine printUbasis(bb)
+    type(Ubasis), intent(in) :: bb
+    integer i,ed
+
+    ed=bb%dimen**2
+
+    do i=1,ed
+        call DISP(bb%bas(i)%idx//' = ',bb%bas(i)%matrix)
+    end do
+
+    return
+end subroutine printUbasis
+
+function cart3d_to_h2(x,y,z) result(rr)
+    type(components) :: rr
+    real, intent(in) :: x,y,z
+    real :: nrm
+
+    nrm=sqrt(x*x+y*y+z*z)
+
+    rr%cmp(1)%idx='f(1,2)'
+    rr%cmp(1)%x=x/nrm
+    rr%cmp(2)%idx='f(2,1)'
+    rr%cmp(2)%x=y/nrm
+    rr%cmp(3)%idx='f(2,2)'
+    rr%cmp(3)%x=z/nrm
+
+end function cart3d_to_h2
+
+
 end module
--- main.f95
+++ main.f95
@@ -6,18 +6,20 @@ use fact
 implicit none
 
 type(Hbasis) :: bb1
+type(Ubasis) :: bb2
+
 call DISP('f = ',f(3,2,3))
 
 call DISP('dirsum = ',dirsum(f(2,3,3),3))
 
-!call DISP('h(3,3) = ',h(3,3))
-
 allocate(bb1%bas(perm(4,2)+4))
 bb1=genHbasis(4)
-!call DISP(bb1%bas(3)%idx//' = ',bb1%bas(3)%matrix)
 
-!call DISP(bb1%bas(2)%idx//' = ',bb1%bas(2)%matrix)
+call printBasis(bb1)
 
-call printHbasis(bb1)
+allocate(bb2%bas(4**2))
+bb2=genUbasis(4)
+
+call printBasis(bb2)
 
 end program
